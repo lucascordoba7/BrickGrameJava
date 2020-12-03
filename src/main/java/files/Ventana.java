55b0515ca83c40b756144787ac20a478e0feb73c
Lucas <Lucas@DESKTOP-U3G13E2>
2020-12-03 12:15:58 -0300

    Primer juego en java, proyecto para hacer un brick game

--- src/main/java/files/Ventana.java
+++ src/main/java/files/Ventana.java
@@ -2,25 +2,9 @@
 package files;
 
 import java.awt.Canvas;
-import java.awt.Color;
 
-import java.awt.Dimension;
 
-import java.awt.FontMetrics;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GraphicsConfiguration;
-import java.awt.Image;
-import java.awt.Paint;
-import java.awt.Rectangle;
-import java.awt.RenderingHints;
-import java.awt.Shape;
-import java.awt.Stroke;
-import java.awt.font.FontRenderContext;
-import java.awt.font.GlyphVector;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferStrategy;
-import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 
